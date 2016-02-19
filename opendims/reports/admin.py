f7e0663abe4503776d1ca915df09db45ac068ece
davidhadrianus89 <davidhadrianus89>
2016-02-19 19:55:39 +0700

    update:action report

--- opendims/reports/admin.py
+++ opendims/reports/admin.py
@@ -2,18 +2,22 @@ from django.contrib import admin
 from django.contrib.gis import admin
 from .models import Disaster, Event, Source
 from .models import Report
-# Register your models here.
+
 
 myModels=[Event,Disaster,Source]
 
+for myModel in myModels:
+	def make_updated(modeladmin, request, queryset):
+		queryset.update(status='u')
+
+	make_updated.short_description="Mark Event as updated"
 
 
 
-class ReportAdmin(admin.ModelAdmin):
-	list_display=["event", 'source', 'status']
-	ordering=['event']
+	class ReportAdmin(admin.ModelAdmin):
+		list_display=["event", 'source', 'status', 'note']
+		ordering=['event']
+		actions=[make_updated]
 	
-
-
 admin.site.register(myModels,admin.OSMGeoAdmin)
 admin.site.register(Report,ReportAdmin)
