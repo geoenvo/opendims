83389b13fa10cd3f1e0ff01542b35a2be60f576e
davidhadrianus89 <davidhadrianus89>
2016-02-22 14:14:12 +0700

    update: add Unverified action

--- opendims/reports/admin.py
+++ opendims/reports/admin.py
@@ -11,16 +11,20 @@ from .models import Report
 
 myModels = [Event,Disaster,Source]
 
+
 for myModel in myModels:
 	def make_unverified(modeladmin, request,queryset):
 		queryset.update(status = 'UN')
 
+
 	make_unverified.short_description = ("Mark Event as Unverified")
+
 
 	def make_verified(modeladmin, request, queryset):
 		queryset.update(status = 'Ver')
 
 	make_verified.short_description = "Mark Even as Verified"
+
 
 	class ReportAdmin(admin.ModelAdmin):
 		list_display = ['event', "source",'created', 'note', 'status']
