8138b862db0cb80e931ece6f8b1631900d5cddbb
davidhadrianus89 <davidhadrianus89>
2016-02-22 13:49:48 +0700

    update:changing __str__() to __unicode__()

--- opendims/reports/admin.py
+++ opendims/reports/admin.py
@@ -1,23 +1,31 @@
 
-#django
-
+#Core Django
 from django.contrib import admin
 from django.contrib.gis import admin
+
+
+#Reports APP
 from .models import Disaster, Event, Source
 from .models import Report
 
-myModels=[Event,Disaster,Source]
+
+myModels = [Event,Disaster,Source]
 
 for myModel in myModels:
-	def make_verified(modeladmin, request, queryset):
-		queryset.update(status='Ver')
+	def make_unverified(modeladmin, request,queryset):
+		queryset.update(status = 'UN')
 
-	make_verified.short_description="Verified"
+	make_unverified.short_description = ("Mark Event as Unverified")
+
+	def make_verified(modeladmin, request, queryset):
+		queryset.update(status = 'Ver')
+
+	make_verified.short_description = "Mark Even as Verified"
 
 	class ReportAdmin(admin.ModelAdmin):
-		list_display=["event", "source",'time', 'note', 'status']
-		ordering=['event']
-		actions=[make_verified]
+		list_display = ['event', "source",'created', 'note', 'status']
+		ordering = ['created']
+		actions = [make_verified, make_unverified]
 	
 admin.site.register(myModels,admin.OSMGeoAdmin)
 admin.site.register(Report,ReportAdmin)
