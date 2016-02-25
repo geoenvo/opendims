cc9c95539c0766589dcf56319e968ce37687fc10
davidhadrianus89 <davidhadrianus89>
2016-02-25 16:01:09 +0700

    poly to polygon and adding new class RT

--- opendims/geolevels/models.py
+++ opendims/geolevels/models.py
@@ -1,5 +1,4 @@
 #Future
-
 from __future__ import unicode_literals
 
 #Core django
@@ -9,60 +8,71 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 class Province(models.Model):
-	name = models.CharField(max_length=50)
-	poly = models.MultiPolygonField(null=True, blank=True)
-	note = models.TextField(null=True)
+    name = models.CharField(max_length=50)
+    polygon = models.MultiPolygonField(null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
 
 
-	def __unicode__(self):
-		return u'%s' %(self.name)
+    def __unicode__(self):
+        return u'%s' %(self.name)
 
 
 class City(models.Model):
-	name = models.CharField(max_length=50)
-	poly = models.MultiPolygonField()
-	province = models.ForeignKey(Province)
-	note = models.TextField(null=True)
+    name = models.CharField(max_length=50)
+    polygon = models.MultiPolygonField(null=True, blank=True)
+    province = models.ForeignKey(Province)
+    note = models.TextField()
 
 
-	def __unicode__(self):
-		return u'%s' %(self.name)
+    def __unicode__(self):
+        return u'%s' %(self.name)
 
 
 class Subdistrict(models.Model):
-	name = models.CharField(max_length=50)
-	poly = models.MultiPolygonField()
-	city = models.ForeignKey(City)
-	note = models.TextField(null=True)
+    name = models.CharField(max_length=50)
+    polygon = models.MultiPolygonField()
+    city = models.ForeignKey(City)
+    note = models.TextField(null=True, blank=True)
 
 
-	def __unicode__(self):
-		return u'%s' %(self.name)
+    def __unicode__(self):
+        return u'%s' %(self.name)
 
 
 class Village(models.Model):
-	id = models.BigIntegerField(primary_key=True)
-	name = models.CharField(max_length=50)
-	poly = models.MultiPolygonField(null=True)
-	subdistrict = models.ForeignKey(Subdistrict)
-	note = models.TextField(null=True)
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    polygon = models.MultiPolygonField()
+    subdistrict = models.ForeignKey(Subdistrict)
+    note = models.TextField(null=True, blank=True)
 
 
-	def __unicode__(self):
-		return u'%s' %(self.name)
+    def __unicode__(self):
+        return u'%s' %(self.name)
 
 
 class RW(models.Model):
-	id = models.BigIntegerField(primary_key=True)
-	name = models.CharField(max_length=50, null=True, blank = True)
-	area = models.DecimalField(max_digits=50, decimal_places=10)
-	poly = models.MultiPolygonField(null=True)
-	village = models.ForeignKey(Village)
-	note = models.TextField(null=True)
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=50, null=True, blank = True)
+    area = models.DecimalField(max_digits=50, decimal_places=10)
+    polygon = models.MultiPolygonField()
+    village = models.ForeignKey(Village)
+    note = models.TextField(null=True, blank=True)
 
 
-	def __unicode__(self):
-		return u'%s' %(self.name)
+    def __unicode__(self):
+        return u'%s' %(self.name)
+
+
+class RT(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    polygon = models.MultiPolygonField()
+    note = models.TextField(null=True, blank =True)
+
+
+    def __unicode__(self):
+    	return u'%s' %(self.name)
 
 
 
