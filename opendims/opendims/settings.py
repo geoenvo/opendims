29c37c90d06cb9db2cdd06d2fecc6818bc7b1fad
davidhadrianus89 <davidhadrianus89>
2016-02-26 15:42:43 +0700

    fix: settings.py

--- opendims/opendims/settings.py
+++ opendims/opendims/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'ckeditor',
-    'ckeditor_uploader'
     'reports',
+    'geolevels',
 ]
 
 MIDDLEWARE_CLASSES = [
