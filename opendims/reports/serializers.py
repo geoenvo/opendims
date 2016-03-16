2b54a944ff6d89f0aaafa94800bf0fed5f96105e
novikesumaningtyas <novi.kesumaningtyas@gmail.com>
2016-03-16 12:04:52 +0700

    update: event serializers

--- opendims/reports/serializers.py
+++ opendims/reports/serializers.py
@@ -1,27 +1,30 @@
 from rest_framework import serializers
 
-
 from .models import Event, Report
 
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
-class EventSerializers(serializers.ModelSerializer):
-        class Meta:
-            model = Event
-            field = ('id',
-                     'disaster',
-                     'point',
-                     'created',
-                     'updated',
-                     'status',
-                     'note',
-                     'height',
-                     'magnitude',
-                     'province',
-                     'city',
-                     'subdistrict',
-                     'village',
-                     'rw',
-                     'rt')
+from geolevels.models import RW
+
+
+class RWSerializers(GeoFeatureModelSerializer):
+    """ A class to serialize locations as GeoJSON compatible data """
+
+    class Meta:
+        model = RW
+        geo_field = 'polygon'
+        id_fields = False
+
+        fields = ()
+
+
+class EventSerializers(GeoFeatureModelSerializer):
+    rw = RWSerializers()
+
+    class Meta:
+        model = Event
+        geo_field = 'point'
+        fields = ('id', 'disaster', 'point', 'created', 'updated', 'status', 'note', 'height', 'magnitude', 'province', 'city', 'subdistrict', 'village', 'rw', 'rt',)
 
 
 class ReportSerializers(serializers.ModelSerializer):
