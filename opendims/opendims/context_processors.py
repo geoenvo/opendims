7cc3bdc352c5d09e035de53d210df2716975fd9f
Seno @ ThinkPad <senoadiw@gmail.com>
2016-04-05 16:58:28 +0700

    update: rearrange base_map template, set default map center from defaults.py

--- opendims/opendims/context_processors.py
+++ opendims/opendims/context_processors.py
@@ -4,6 +4,7 @@ from django.conf import settings
 def resource_urls(request):
     resources_urls = dict(
         SITE_NAME=settings.SITE_NAME,
+        DEFAULT_CENTER=settings.LEAFLET_CONFIG['DEFAULT_CENTER'],
     )
 
     return resources_urls
