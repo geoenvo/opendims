91ff8675f729229f452e807620475bd572653e5d
Seno @ ThinkPad <senoadiw@gmail.com>
2016-04-10 00:30:28 +0700

    fix: event map center and zoom

--- opendims/opendims/context_processors.py
+++ opendims/opendims/context_processors.py
@@ -5,6 +5,7 @@ def resource_urls(request):
     resources_urls = dict(
         SITE_NAME=settings.SITE_NAME,
         DEFAULT_CENTER=settings.LEAFLET_CONFIG['DEFAULT_CENTER'],
+        DEFAULT_ZOOM=settings.LEAFLET_CONFIG['DEFAULT_ZOOM'],
     )
 
     return resources_urls
