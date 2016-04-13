c914f8aaa1d0964d0c9335fdc86502a2cea761fe
Seno @ ThinkPad <senoadiw@gmail.com>
2016-04-13 07:16:36 +0700

    update: event_map cleanup

--- opendims/opendims/settings/defaults.py
+++ opendims/opendims/settings/defaults.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from django.core.urlresolvers import reverse_lazy
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -183,7 +185,11 @@ ADMIN_REORDER = (
 
 SITE_NAME = 'Open-DiMS'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = reverse_lazy('login')
+
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+
+LOGOUT_URL = reverse_lazy('logout')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
