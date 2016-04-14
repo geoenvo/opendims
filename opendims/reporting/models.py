b8750d624299ab12090f784362c73826f3ba6627
Seno @ ThinkPad <senoadiw@gmail.com>
2016-04-14 12:32:19 +0700

    fix: reporting models code review

--- opendims/reporting/models.py
+++ opendims/reporting/models.py
@@ -16,16 +16,18 @@ verbose_created = _('Created')
 verbose_updated = _('Updated')
 verbose_author = _('Author')
 verbose_content = _('Content')
+verbose_disaster_attached = _('Attached disaster')
 verbose_template = _('Template')
-verbose_text_author = _('Text_author')
-verbose_text_title = _('Text_title')
+verbose_text_id = _('Text ID')
+verbose_text_author = _('Text author')
+verbose_text_title = _('Text title')
 verbose_file = _('File')
 verbose_type = _('Type')
+verbose_status = _('Status')
 verbose_report = _('Report')
 
 
 class Template(CommonAbstractModel):
-
     name = models.CharField(max_length=100, verbose_name=verbose_name)
     created = models.DateTimeField(
         default=timezone.now,
@@ -39,7 +41,9 @@ class Template(CommonAbstractModel):
     disaster_attached = models.ManyToManyField(
         Disaster,
         blank=True,
-        related_name='template_attached')
+        related_name='template_attached',
+        verbose_name=verbose_disaster_attached
+    )
 
     class Meta:
         ordering = ['-updated', '-created']
@@ -50,14 +54,12 @@ class Template(CommonAbstractModel):
 
 
 class Report(CommonAbstractModel):
-
-    PERIOD_CHOICES = (
+    TYPE_CHOICES = (
         ('DAILY', _('Daily')),
         ('WEEKLY', _('Weekly')),
         ('MONTHLY', _('Monthly')),
         ('YEARLY', _('Yearly')),
     )
-
     STATUS_CHOICES = (
         ('ACTIVE', _('Active')),
         ('INACTIVE', _('Inactive')),
@@ -65,39 +67,54 @@ class Report(CommonAbstractModel):
 
     template = models.ForeignKey(
         Template,
-        verbose_name=verbose_template,
-        related_name='reports')
+        related_name='reports',
+        verbose_name=verbose_template
+    )
     created = models.DateTimeField(
          default=timezone.now,
-         verbose_name=verbose_created)
+         verbose_name=verbose_created
+     )
     type = models.CharField(
         max_length=100,
-        choices=PERIOD_CHOICES,
-        default='DAILY')
+        choices=TYPE_CHOICES,
+        default='DAILY',
+        verbose_name=verbose_type
+    )
     updated = models.DateTimeField(
         auto_now=True,
-        verbose_name=verbose_updated)
+        verbose_name=verbose_updated
+    )
     author = models.ForeignKey(
         'auth.User',
-        null=True,
-        blank=False)
+    )
     content = RichTextField(
         blank=True,
-        verbose_name=verbose_content)
+        verbose_name=verbose_content
+    )
+    text_id = models.TextField(
+        blank=True,
+        verbose_name=verbose_text_id
+    )
     text_author = models.TextField(
         blank=True,
-        null=True,
-        verbose_name=verbose_text_author)
+        verbose_name=verbose_text_author
+    )
     text_title = models.TextField(
         blank=True,
-        null=True,
-        verbose_name=verbose_text_title)
-
+        verbose_name=verbose_text_title
+    )
     status = models.CharField(
         max_length=50,
         choices=STATUS_CHOICES,
-        default='ACTIVE')
-    file = models.FileField(blank=True, upload_to='reporting/report')
+        default='ACTIVE',
+        verbose_name=verbose_status
+    )
+    file = models.FileField(
+        null=True,
+        blank=True,
+        upload_to='reporting/report/',
+        verbose_name=verbose_file
+    )
 
     class Meta:
         ordering = ['-updated', '-created']
@@ -120,16 +137,20 @@ class Report(CommonAbstractModel):
 
 class Attachment(CommonAbstractModel):
     report = models.ForeignKey(
-        Report, verbose_name=verbose_report,
-        related_name='templates')
+        Report,
+        related_name='attachments',
+        verbose_name=verbose_report
+    )
     created = models.DateTimeField(
          default=timezone.now,
-         verbose_name=verbose_created)
+         verbose_name=verbose_created
+     )
     file = models.FileField(
         blank=True,
         null=True,
-        upload_to='reporting/attachment',
-        verbose_name=verbose_file)
+        upload_to='reporting/attachment/',
+        verbose_name=verbose_file
+    )
 
     class Meta:
         ordering = ['-report']
