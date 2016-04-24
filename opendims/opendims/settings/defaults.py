eb32a4700b682d65f22fa3b77dd9d23d729d3e90
novikesumaningtyas <novi.kesumaningtyas@gmail.com>
2016-04-24 21:18:01 +0700

    add:weather icons in bower_installed_app

--- opendims/opendims/settings/defaults.py
+++ opendims/opendims/settings/defaults.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
     'reporting',
     'geolevels',
     'waterlevel',
+    'smsblast',
+    'weatherforecast',
+    'automaticweathersystem',
     'maps',
     'jaksafe',
     'django_crontab',
@@ -176,6 +179,10 @@ BOWER_INSTALLED_APPS = (
     'leaflet.locatecontrol#0.43.0',
     'leaflet-groupedlayercontrol#0.3.0',
     'eonasdan-bootstrap-datetimepicker#4.17.37',
+    'animate.css#3.5.1',
+    'jquery-migrate#1.2.1',
+    'modernizr#2.0.6',
+    'weather-icons#2.0.10',
 )
 
 # App model reordering in /admin page
