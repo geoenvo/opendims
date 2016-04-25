7e14a9c22da4db8ace7e5c4da48e8487d7f86f43
davidhadrianus89 <davidhadrianus89@gmail.com>
2016-04-25 10:28:27 +0700

    update : add function to render html to pdf

--- opendims/reporting/admin.py
+++ opendims/reporting/admin.py
@@ -1,10 +1,25 @@
 from __future__ import unicode_literals
+import datetime
 
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 from django.contrib.gis import admin
+from django.template.loader import get_template
+from django.core.files.temp import NamedTemporaryFile
+from django.core.files import File
+
+from xhtml2pdf import pisa
 
 from .models import Template, Report, Attachment
+
 from .forms import ReportForm
+
+from reports.models import Event
+
+verbose_template_details = _('Template details')
+verbose_report_details = _('Report details')
+verbose_set_active = _('Set Template as Active')
+verbose_set_inactive = _('Set Template as Inactive')
 
 
 verbose_report_details = _('Report details')
@@ -20,8 +35,66 @@ class AttachmentInline(admin.TabularInline):
     extra = 3
 
 
+class AttachmentInLine (admin.TabularInline):
+    model = Attachment
+    extra = 1
+
+
 class ReportAdmin(admin.ModelAdmin):
+
     form = ReportForm
+
+    def save_model(self, request, obj, form, change):
+        """
+        """
+        obj.save()
+
+        for disaster in obj.template.disaster_attached.all():
+
+            try:
+                template = get_template('reporting/report_' + disaster.code + '.html')
+                print disaster.code
+                created_start = datetime.datetime(
+                    obj.created.year,
+                    obj.created.month,
+                    obj.created.day,
+                    0
+                )
+                created_end = datetime.datetime(
+                    obj.created.year,
+                    obj.created.month,
+                    obj.created.day,
+                    24
+                )
+                events = Event.objects.filter(
+                    disaster__code=disaster.code,
+                    created__range=(created_start, created_end)
+                )
+                print 'hello', '+', events
+                html = template.render({'events': events})
+                print html
+                pdf_disaster_temp = NamedTemporaryFile()
+
+                pisa.CreatePDF(
+                    html.encode('utf-8'),
+                    dest=pdf_disaster_temp,
+                    encoding='utf-8'
+                )
+                attachment_created = timezone.localtime(timezone.now())
+                pdf_disaster_filename = "{}__{}{}".format(
+                    attachment_created.strftime('%Y%m%d-%H%M'),
+                    disaster.code,
+                    '.pdf'
+                )
+
+                attachment = Attachment()
+                attachment.report = obj
+                attachment.created = attachment_created
+                attachment.file.save(pdf_disaster_filename, File(pdf_disaster_temp), save=False)
+                attachment.save()
+            except:
+                pass
+
     fieldsets = [
         (verbose_report_details, {
             'fields': [
@@ -62,6 +135,9 @@ class ReportAdmin(admin.ModelAdmin):
     list_filter = ['template', 'created', 'updated']
     search_fields = ['author', 'file', 'template']
     inlines = [AttachmentInline]
+    list_filter = ['content', 'template', 'created', 'updated']
+    search_fields = ['file', 'author']
+    inlines = [AttachmentInLine]
 
 
 class TemplateAdmin(admin.ModelAdmin):
