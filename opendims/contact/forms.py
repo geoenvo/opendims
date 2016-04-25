b78e31e3c52807921246bc60f8ef6bae5c868874
Zafkiel 12 <bayutri.wibowougm@gmail.com>
2016-04-25 11:37:34 +0700

    update: adding contact captcha, fixing jaksafe template, fixing contact url

--- opendims/contact/forms.py
+++ opendims/contact/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-
+from captcha.fields import CaptchaField
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+    captcha = CaptchaField()
     class Meta:
         model = Contact
         fields = ('name', 'email', 'website', 'subject', 'message',)
--- opendims/opendims/settings/defaults.py
+++ opendims/opendims/settings/defaults.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'jaksafe',
     'django_crontab',
     'contact',
+    'captcha',
 ]
 
 MIDDLEWARE_CLASSES = [
