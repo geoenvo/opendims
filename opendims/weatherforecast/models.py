5e521a15e1833466005e785a043254850a5f6792
novikesumaningtyas <novi.kesumaningtyas@gmail.com>
2016-04-26 19:30:43 +0700

    update:template

--- opendims/weatherforecast/models.py
+++ opendims/weatherforecast/models.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.gis.db import models
 from geolevels.models import City, Province
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 from common.models import CommonAbstractModel
 
@@ -39,55 +41,63 @@ class WeatherForecastReport(CommonAbstractModel):
     forecast_morning = models.CharField(
         max_length=50,
         choices=FORECAST_CHOICES,
-        default='CERAH',
-        null=True,
+        blank=True,
         verbose_name=verbose_forecastmorning
     )
     forecast_noon = models.CharField(
         max_length=50,
         choices=FORECAST_CHOICES,
-        default='CERAH',
-        null=True,
+        blank=True,
         verbose_name=verbose_forecastnoon
     )
     forecast_night = models.CharField(
         max_length=50,
         choices=FORECAST_CHOICES,
-        default='CERAH',
-        null=True,
+        blank=True,
         verbose_name=verbose_forecastnight
     )
     forecast = models.CharField(
         max_length=50,
         choices=FORECAST_CHOICES,
-        default='CERAH',
-        null=True,
+        blank=True,
         verbose_name=verbose_forecast
     )
     city = models.ForeignKey(
         City,
         null=True,
+        blank=True,
         verbose_name=verbose_city
     )
     province = models.ForeignKey(
         Province,
         null=True,
+        blank=True,
         verbose_name=verbose_province
     )
-    temperature_min = models.PositiveIntegerField(
-        default=0,
+    temperature_min = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=4,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0'))],
         verbose_name=verbose_tempmin
     )
-    temperature_max = models.PositiveIntegerField(
-        default=0,
+    temperature_max = models.DecimalField(
+        null=True,
+        blank=True,
+        max_digits=4,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0'))],
         verbose_name=verbose_tempmax
     )
     humidity_min = models.PositiveIntegerField(
-        default=0,
+        null=True,
+        blank=True,
         verbose_name=verbose_humidmin
     )
     humidity_max = models.PositiveIntegerField(
-        default=0,
+        null=True,
+        blank=True,
         verbose_name=verbose_humidmax
     )
     note = models.TextField(blank=True, verbose_name=verbose_note)
