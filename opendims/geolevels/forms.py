509db25b3010f5f28264b4e8687f6bbc79324bed
Seno @ ThinkPad <senoadiw@gmail.com>
2016-04-26 16:07:42 +0700

    update: id length validation for geolevels

--- opendims/geolevels/forms.py
+++ opendims/geolevels/forms.py
@@ -1,17 +1,47 @@
+from __future__ import unicode_literals
+
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from dal import autocomplete
 
 from .models import Province, City, Subdistrict, Village, RW, RT
 
 
-class ProvinceForm(forms.ModelForm):
+class GeolevelsForm(forms.ModelForm):
+    class Media:
+        js = (
+            'geolevels/js/admin_geolevels.js',
+        )
+
+    def clean_id(self):
+        id = self.cleaned_data.get('id', None)
+        min_id_length = 0
+        error = False
+        if isinstance(self, CityForm):
+            min_id_length = 4
+        elif isinstance(self, SubdistrictForm):
+            min_id_length = 7
+        elif isinstance(self, VillageForm):
+            min_id_length = 10
+        elif isinstance(self, RWForm):
+            min_id_length = 13
+        elif isinstance(self, RTForm):
+            min_id_length = 16
+        if len(str(id)) < min_id_length:
+            error = True
+        if error:
+            error_message = _("ID digit length must be at least")
+            raise forms.ValidationError("{}: {}".format(error_message, min_id_length))
+
+
+class ProvinceForm(GeolevelsForm):
     class Meta:
         model = Province
         fields = '__all__'
 
 
-class CityForm(forms.ModelForm):
+class CityForm(GeolevelsForm):
     class Meta:
         model = City
         fields = '__all__'
@@ -22,7 +52,7 @@ class CityForm(forms.ModelForm):
         }
 
 
-class SubdistrictForm(forms.ModelForm):
+class SubdistrictForm(GeolevelsForm):
     class Meta:
         model = Subdistrict
         fields = '__all__'
@@ -33,7 +63,7 @@ class SubdistrictForm(forms.ModelForm):
         }
 
 
-class VillageForm(forms.ModelForm):
+class VillageForm(GeolevelsForm):
     class Meta:
         model = Village
         fields = '__all__'
@@ -44,7 +74,7 @@ class VillageForm(forms.ModelForm):
         }
 
 
-class RWForm(forms.ModelForm):
+class RWForm(GeolevelsForm):
     class Meta:
         model = RW
         fields = '__all__'
@@ -55,7 +85,7 @@ class RWForm(forms.ModelForm):
         }
 
 
-class RTForm(forms.ModelForm):
+class RTForm(GeolevelsForm):
     class Meta:
         model = RT
         fields = '__all__'
