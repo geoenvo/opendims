c07ab79d456102114f9e08d843d84a70f7d267ac
novikesumaningtyas <novi.kesumaningtyas@gmail.com>
2016-05-13 18:53:05 +0700

    add:waterlevel info in home page

--- opendims/waterlevel/templatetags/waterlevel_tags.py
+++ opendims/waterlevel/templatetags/waterlevel_tags.py
@@ -1,14 +1,30 @@
 from django import template
+import datetime
 
-from waterlevel.models import WaterLevelReport
+from django.utils import timezone
+from waterlevel.models import WaterLevelReport, WaterGate
 
 register = template.Library()
-
 
 @register.simple_tag
 def get_waterlevelreport(watergate, now):
     waterlevelreports = WaterLevelReport.objects.filter(
         watergate=watergate,
         created__date=(now.date())
-    ).order_by('created')
+    ).order_by('-created')
     return waterlevelreports
+
+@register.simple_tag
+def get_watergate():
+    watergates = WaterGate.objects.filter(
+    ).order_by('name')
+    return watergates
+ 
+@register.simple_tag
+def get_waterlevelreport_block(watergate):
+    now = timezone.localtime(timezone.now())
+    waterlevelreports = WaterLevelReport.objects.filter(
+        watergate=watergate,
+        created__date=(now.date())
+    ).order_by('-created')
+    return waterlevelreports 
