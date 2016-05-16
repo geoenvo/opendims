7e7a071f1dab092f25c1f8b87769b673f720bdf3
Seno @ ThinkPad <senoadiw@gmail.com>
2016-05-16 16:13:40 +0700

    fix: Event is no longer mandatory for creating a Report, new: fixtures for ReportAggregator

--- opendims/reportaggregator/cron.py
+++ opendims/reportaggregator/cron.py
@@ -6,7 +6,7 @@ from django.conf import settings
 import tweepy
 from tweepy import OAuthHandler
 
-from reports.models import Report, Event, Source as Report_Source
+from reports.models import Report, Source as Reports_Source
 from .models import Source
 
 
@@ -15,34 +15,30 @@ def report_scheduled_job():
     consumer_secret = settings.CONSUMER_SECRET
     access_token = settings.ACCESS_TOKEN
     access_secret = settings.ACCESS_SECRET
+    max_tweets = settings.MAX_TWEETS
 
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
-
     api = tweepy.API(auth)
 
-    max_tweets = 100
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-    sources = Source.objects.filter(status='ACTIVE')
+    sources = Source.objects.filter(status='ACTIVE', type='TWITTER')
 
     for active_source in sources:
         keywords = active_source.keywords.all()
         for a_keyword in keywords:
             query = a_keyword.keyword
             searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+            source = Reports_Source.objects.get(code='TWT')
             for status in searched_tweets:
                 tweet_localcreatedtime = status.created_at + timedelta(hours=7)
                 diff = datetime.now() - tweet_localcreatedtime
-                diff_hours = diff.total_seconds()/3600
+                diff_hours = diff.total_seconds() / 3600
                 if diff_hours <= 1:
-                    new_event_create = Event(
-                        disaster=active_source.disaster
-                    )
-                    new_event_create.save()
                     note = status.text.translate(non_bmp_map)
-                    new_report_create = Report(
+                    note = "[{}] {}".format(active_source.disaster, note)
+                    new_report = Report(
                         note=note,
-                        event=new_event_create,
-                        source=Report_Source.objects.get(code='TWT')
+                        source=source
                     )
-                    new_report_create.save()
+                    new_report.save()
