3a694528e68915de67ca3dd5620386ad6abab844
Seno @ ThinkPad <senoadiw@gmail.com>
2016-05-26 12:39:37 +0700

    fix: disaster rehab search by location form layout

--- opendims/disasterrehabilitation/forms.py
+++ opendims/disasterrehabilitation/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 from dal import autocomplete
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
+from crispy_forms.layout import Layout, Submit, Div
 
 from reports.forms import CommonEventForm
 from .models import Location
@@ -40,33 +40,47 @@ class LocationForm(CommonEventForm):
         }
 
 
+verbose_province = _('Province')
+verbose_city = _('City')
+verbose_subdistrict = _('Subdistrict')
+verbose_village = _('Village')
+verbose_rw = _('RW')
+verbose_rt = _('RT')
+
+
 class SearchByLocationForm(forms.Form):
     province = forms.ModelChoiceField(
+        label=verbose_province,
         required=False,
         queryset=Province.objects.all(),
         widget=autocomplete.ModelSelect2(url='geolevels:autocomplete_province')
     )
     city = forms.ModelChoiceField(
+        label=verbose_city,
         required=False,
         queryset=City.objects.all(),
         widget=autocomplete.ModelSelect2(url='geolevels:autocomplete_city', forward=['province']),
     )
     subdistrict = forms.ModelChoiceField(
+        label=verbose_subdistrict,
         required=False,
         queryset=Subdistrict.objects.all(),
         widget=autocomplete.ModelSelect2(url='geolevels:autocomplete_subdistrict', forward=['city']),
     )
     village = forms.ModelChoiceField(
+        label=verbose_village,
         required=False,
         queryset=Village.objects.all(),
         widget=autocomplete.ModelSelect2(url='geolevels:autocomplete_village', forward=['subdistrict']),
     )
     rw = forms.ModelChoiceField(
+        label=verbose_rw,
         required=False,
         queryset=RW.objects.all(),
         widget=autocomplete.ModelSelect2(url='geolevels:autocomplete_rw', forward=['village']),
     )
     rt = forms.ModelChoiceField(
+        label=verbose_rt,
         required=False,
         queryset=RT.objects.all(),
         widget=autocomplete.ModelSelect2(url='geolevels:autocomplete_rt', forward=['rw']),
@@ -77,14 +91,27 @@ class SearchByLocationForm(forms.Form):
         self.helper = FormHelper()
         self.helper.form_method = 'get'
         self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-2'
-        self.helper.field_class = 'col-lg-8'
+        self.helper.label_class = 'col-md-3'
+        self.helper.field_class = 'col-md-9'
         self.helper.layout = Layout(
-            'province',
-            'city',
-            'subdistrict',
-            'village',
-            'rw',
-            'rt',
-            Submit('', _('Search')),
+            Div(Div(
+                    'province',
+                    'city',
+                    'subdistrict',
+                    css_class='col-md-6'
+                ),
+                Div(
+                    'village',
+                    'rw',
+                    'rt',
+                    css_class='col-md-6'
+                ),
+                css_class='row'
+            ),
+            Div(Div(
+                    Submit('', _('Search')),
+                    css_class='col-md-12 text-right'
+                ),
+                css_class='row'
+            )
         )
