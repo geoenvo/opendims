5c8a3b5238d9b8479292a0f0378ee23099356911
davidhadrianus89 <davidhadrianus89@gmail.com>
2016-06-23 15:42:14 +0700

    update: reporting app

--- opendims/reporting/urls.py
+++ opendims/reporting/urls.py
@@ -7,11 +7,3 @@ from . import views
 urlpatterns = [
     url(r'^test_screamshot/$', views.test_screamshot, name='test_screamshot')
 ]
-
-urlpatterns = [
-    url(r'^statistics/$', views.statistics, name='statistics'),
-    url(r'^weather_forecast/$', views.weather_forecast, name='weather_forecast'),
-    url(r'^waterlevel/$', views.waterlevel, name='waterlevel'),
-    url(r'^report_ABR/$', views.report_ABR, name='report_ABR')
-
-]
