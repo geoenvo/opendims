2d39838a2c40a236d705d5db2439fb562fad6247
davidhadrianus89 <davidhadrianus89@gmail.com>
2016-07-12 13:07:15 +0700

    update: resolving app name ambiguous in admin (issue #20)

--- opendims/reporting/apps.py
+++ opendims/reporting/apps.py
@@ -6,4 +6,4 @@ from django.utils.translation import gettext_lazy as _
 
 class ReportingConfig(AppConfig):
     name = 'reporting'
-    verbose_name = _('Reporting')
+    verbose_name = _('Pusdalops Reporting')
